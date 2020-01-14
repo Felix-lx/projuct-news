5bfeb5b24999a96de55bc55f1f4b0be8e6c8bea5
Felix-lx <417732673@qq.com>
2020-01-14 18:30:50 +0800

    login页面进行中1/1418:30

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './styles/iconfont.css'
+import './styles/reset-mobile.css'
+import 'lib-flexible'
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="login">
+    <em class="iconfont iconicon-test"></em>
+    <div class="username">
+        <input type="text" placeholder="请输入用户名/手机号">
+    </div>
+    <div class="passsword">
+        <input type="password" placeholder="请输入密码">
+    </div>
+    <div class="btn">
+        <button>登陆</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
